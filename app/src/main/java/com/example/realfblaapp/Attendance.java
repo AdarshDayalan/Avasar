2b9827bfef43673edbc3b3fa9f71bcb6f151124f
adars <adarshdayal7@gmail.com>
2019-12-26 00:04:48 -0600

    Locally saves and loads student id so now students don't need to constantly input their id numbers

--- app/src/main/java/com/example/realfblaapp/Attendance.java
+++ app/src/main/java/com/example/realfblaapp/Attendance.java
@@ -2,36 +2,127 @@ package com.example.realfblaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Attendance extends AppCompatActivity {
+    private static final String FILE_NAME = "example.txt";
+    EditText idTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.attendance);
-        Button checkInBtn = (Button) findViewById(R.id.checkInButton);
-        checkInBtn.setOnClickListener(new View.OnClickListener() {
 
+        idTxt = findViewById(R.id.studentId);
+        load(idTxt);
+
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if(nfcAdapter != null && nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC available", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+        }
+
+
+        Button checkInBtn = (Button) findViewById(R.id.checkInButton);
+
+        checkInBtn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               EditText idTxt = findViewById(R.id.studentId);
-               int idNum = Integer.parseInt(idTxt.getText().toString());
-               TextView timeTxt = findViewById(R.id.time);
+               idTxt = findViewById(R.id.studentId);
 
-               SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
-               String currentDateAndTime = sdf.format(new Date());
-               timeTxt.setText(currentDateAndTime + ", " + idNum);
+               if (idTxt != null && idTxt.length() == 6 ) {
+                   save(idTxt);
+
+                   TextView timeTxt = findViewById(R.id.time);
+
+                   SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+                   String currentDateAndTime = sdf.format(new Date());
+                   timeTxt.setText(currentDateAndTime + ", " + idTxt.getText().toString());
+
+               }
+               else{
+                   Toast.makeText(getApplicationContext(), "Invalid ID number. Try Again", Toast.LENGTH_LONG).show();
+               }
+
            }
        });
 
-    }}
+    }
+
+    public void save(View v) {
+
+        String text = idTxt.getText().toString();
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+
+//            Toast.makeText(this, "Saved to" + getFilesDir() + "/" + FILE_NAME,
+//                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void load(View v) {
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("");
+            }
+
+            idTxt.setText(sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
