1ea798367fface22c4d984677270050746514735
adars <adarshdayal7@gmail.com>
2019-12-25 18:52:18 -0600

    Added Check In button functionality. Outputs student Id with date and time

--- app/src/main/java/com/example/realfblaapp/Attendance.java
+++ app/src/main/java/com/example/realfblaapp/Attendance.java
@@ -5,6 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Attendance extends AppCompatActivity {
 
@@ -17,7 +24,13 @@ public class Attendance extends AppCompatActivity {
 
            @Override
            public void onClick(View v) {
+               EditText idTxt = findViewById(R.id.studentId);
+               int idNum = Integer.parseInt(idTxt.getText().toString());
+               TextView timeTxt = findViewById(R.id.time);
 
+               SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+               String currentDateAndTime = sdf.format(new Date());
+               timeTxt.setText(currentDateAndTime + ", " + idNum);
            }
        });
 
