0be0afce2929bcf1213eacb2433c6590c20ab6f4
yuvchau <yuvchau@gmail.com>
2020-01-02 15:16:39 -0600

    Added Local Officer Page and Button

--- app/src/main/java/com/example/realfblaapp/About.java
+++ app/src/main/java/com/example/realfblaapp/About.java
@@ -3,9 +3,11 @@ package com.example.realfblaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
 
@@ -41,5 +43,10 @@ public class About extends AppCompatActivity {
 
     }
 
-
+    public void officer(View view) {
+        Intent officerIntent = new Intent(this, Officers.class);
+        startActivity(officerIntent);
+    }
 }
+
+
