a71ecd69ee997e662b8e805dbd6549fe21a5fe04
adars <adarshdayal7@gmail.com>
2020-05-26 12:52:58 -0500

    Implemented Crashlytics

--- app/src/main/java/com/example/realfblaapp/AttendanceData.java
+++ app/src/main/java/com/example/realfblaapp/AttendanceData.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.crashlytics.BuildConfig;
 
 import java.util.ArrayList;
 import java.util.Map;
