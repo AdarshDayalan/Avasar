649bc6e7cf893022faed23458203ef45bceae633
adars <adarshdayal7@gmail.com>
2020-05-26 15:44:40 -0500

    Cleaned up attendance

--- app/src/main/java/com/example/realfblaapp/AttendenceDatabase.java
+++ app/src/main/java/com/example/realfblaapp/AttendenceDatabase.java
@@ -1,5 +1,9 @@
 package com.example.realfblaapp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AttendenceDatabase {
     String attendanceId;
     String attendanceTimeDate;
